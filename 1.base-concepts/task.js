3158ff514e05c0531fb7e63ac35ee28fd68a6678
radik2022 <ibrakovrm@mail.ru>
2023-04-16 17:13:48 +0300

    Ferst commit

--- 1.base-concepts/task.js
+++ 1.base-concepts/task.js
@@ -1,10 +1,17 @@
 "use strict"
 function solveEquation(a, b, c) {
   let arr = [];
-  
-  return arr;
+  let d;
+  d = Math.pow(b, 2) - 4 * a * c;
+  if (d < 0) {
+      return [];
+  }
+  else if(d === 0) {
+      return [-b / (2 * a)];
+  } else if (d > 0) {
+      return [(-b + Math.sqrt(d)) / (2 * a), (-b - Math.sqrt(d)) / (2 * a)];
+  }
 }
-
 function calculateTotalMortgage(percent, contribution, amount, countMonths) {
   
 }
